657c4ef47eb209f520b019f864d1b448dce7ce06
rowsim <rowanq@live.co.uk>
2019-06-16 18:38:50 +0100

    added code splitting

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import MainChat from "./pages/main-chat/main-chat";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import asyncComponent from "./components/util/AsyncComponent";
 import "./styles/main.scss";
+
+const AsyncMainChat = asyncComponent(() =>
+  import("./pages/main-chat/main-chat")
+);
+const AsyncLoginPage = asyncComponent(() => import("./pages/login/login-page"));
 
 function MainRouter() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={MainChat} />
-        <Route component={MainChat} />
+        <Route exact path="/chat" component={AsyncMainChat} />
+        <Route component={AsyncLoginPage} />
       </Switch>
     </Router>
   );
--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-function LoginPage() {}
+function LoginPage() {
+    return (
+        <div>Login!</div>
+    )
+}
 
 export default LoginPage;
