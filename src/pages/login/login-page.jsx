58058d763d0006b1582db9d020d9962e5ed2a5cf
rowsim <rowanq@live.co.uk>
2019-06-16 22:34:25 +0100

    added login page with google sign in & vantajs background

---
+++ src/pages/login/login-page.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import "./login-page.scss";
+
+const clientId =
+  "317143211460-nt9t4cirb5qb2uhmrr3rc34uqu18cpth.apps.googleusercontent.com";
+const responseGoogle = response => {
+  console.log(response);
+};
+
+class LoginPage extends React.Component {
+  componentDidMount() {
+    this.effect = window.VANTA.BIRDS({
+      el: "#vanta-bg",
+      backgroundColor: 0x3e061d,
+      backgroundAlpha: 0.95,
+      color1: 0xff8700,
+      color2: 0x6900ff,
+      wingSpan: 35.0,
+      speedLimit: 6.0,
+      separation: 60.0,
+      alignment: 30.0,
+      quantity: 3.0
+    });
+  }
+  componentWillUnmount() {
+    if (this.effect) this.effect.destroy();
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="vanta-bg" />
+        <div className="login-page--title">
+          <h1>react messenger</h1>
+          <h3>using:</h3>
+          <a href="https://www.npmjs.com/package/create-react-app" target="_blank" rel="noopener noreferrer" >@ react</a>
+          <a href="https://www.npmjs.com/package/node-sass" target="_blank" rel="noopener noreferrer" >@ node-sass</a>
+          <a href="https://www.npmjs.com/package/@pusher/chatkit-client" target="_blank" rel="noopener noreferrer" >@ pusher/chatkit-client</a>
+          <a href="https://www.npmjs.com/package/react-google-login" target="_blank" rel="noopener noreferrer" >@ react-google-login</a>
+          <a href="https://www.npmjs.com/package/react-loader-spinner" target="_blank" rel="noopener noreferrer" >@ react-loader-spinner</a>
+        </div>
+        <div className="login-page">
+          <div className="login-page__google">
+            <p>Sign in with Google.</p>
+            <GoogleLogin
+              clientId={clientId}
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
