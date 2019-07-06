71966aec5d5dcbefabce85c6d75057cfae42a5e5
rowsim <rowanq@live.co.uk>
2019-07-07 00:39:33 +0100

    added leave butotn css

--- src/components/room/RoomStatus.tsx
+++ src/components/room/RoomStatus.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './RoomStatus.scss';
 
 interface RoomStatusProps {
   room: {
@@ -16,19 +17,19 @@ export default class RoomStatus extends React.Component<RoomStatusProps> {
     if (this.props.room.users) {
       return (
         <div className="room-status">
-          <div className="room-status-info">
+          <div className="room-status__info">
             <div>{this.props.room.name}</div>
-            <div className="room-status-users">
+            <div className="room-status__users">
               {this.props.room.users.map((user, index) => {
                 return (
-                  <div key={index} className="room-status-user">
+                  <div key={index} className="room-status__user">
                     {user.name + ","}
                   </div>
                 );
               })}
             </div>
           </div>
-          <div>Leave</div>
+          <button className="room-status__leave rc-button">Leave</button>
         </div>
       );
     }
